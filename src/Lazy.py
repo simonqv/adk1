b273bc776573ff8f6bec42f2193f49f8e4ec4d13
Simon Larspers Qvist <simonqv@kth.se>
2021-09-15 15:18:55 +0200

    Add files via upload

---
+++ src/Lazy.py
@@ -0,0 +1,15 @@
+def lazy(search):
+    lazy_hash = ord(search[0]) * 900 + ord(search[1]) * 30 + ord(search[2])
+    print(lazy_hash)
+
+    # Hasha alla ord.
+    # skriv alla ord i fil. (en gång per ord) och sedan alla index de förekommer på.
+    # Korpus har väl alla?
+    return
+
+
+def main():
+    lazy("ööö")
+
+
+main()
